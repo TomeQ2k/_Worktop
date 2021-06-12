e5690858bbd92bbdde77e69d7ae21b0ec8c83d08
tomeq <tomeknowok@gmail.com>
2021-06-12 11:27:34 +0200

    .

--- wwwroot/js/message.js
+++
@@ -1,8 +0,0 @@
-class Message {
-    constructor(text, senderName, senderId, roomId) {
-        this.text = text;
-        this.senderName = senderName;
-        this.senderId = senderId;
-        this.chatRoomId = roomId;
-    }
-}
